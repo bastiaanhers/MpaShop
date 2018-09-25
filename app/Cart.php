21c2f1f47dad1591fc6bb322c200ede84455b85f
bastiaan hersman <bastiaan.hers@gmail.com>
2018-09-25 11:43:15 +0200

    fire alert

--- app/Cart.php
+++ app/Cart.php
@@ -7,14 +7,12 @@ use Session;
 class Cart
 {
     public $items= [];
-    public $totalAmount = 0;
 
 
     public function __construct(){
         $oldCart = Session::has('cart') ? Session::get('cart') : null;
         if ($oldCart){
             $this->items = $oldCart->items;
-            $this->totalAmount = $oldCart->totalAmount;
         }
     }
 
@@ -28,16 +26,13 @@ class Cart
                    $storedItem = $this->items[$i];
                    $storedItem['amount']++;
                    $this->items[$i] = $storedItem;
-                   $this->totalAmount++;
                    $idIsThere = true;
                }
             }
             if(!$idIsThere){
-            $this->totalAmount++;    
             array_push($this->items, $storedItem);
             }
         }else{ 
-            $this->totalAmount++;    
             array_push($this->items, $storedItem);
         }
     }
@@ -49,13 +44,12 @@ class Cart
         if (count($cartForDel->items) > 1) {
            for($i= 0; $i< count($cartForDel->items); $i++) {
                if($cartForDel->items[$i]['itemId'] == $delId){
-                   array_splice($cartForDel->items, $i, $i);
+                   array_splice($cartForDel->items, $i, 1);
                }
             }
         }else{
             unset($cartForDel->items[0]);
         }
-        $cartForDel->totalAmount --;
         session()->put('cart', $cartForDel);
     }
     public static function editAmount($editId, $newAmount) {
